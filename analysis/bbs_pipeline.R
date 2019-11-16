f533bce05915443c4e2ec3d46bbd71f8a9f3140e
Renata Diaz <diaz.renatam@gmail.com>
2019-11-16 08:20:36 -0500

    go ahead and use MATSS in pipelinme

--- analysis/bbs_pipeline.R
+++ analysis/bbs_pipeline.R
@@ -6,14 +6,18 @@ expose_imports(cats)
 ndraws = 100
 set.seed(1977)
 
-#bbs_dats <- MATSS::build_bbs_datasets_plan(data_subset = c(1:2))
-rts <- c(1, 268, 314)
-rgs <- c(11, 8, 27)
 
-bbs_dats <- drake_plan(
-  bbs_data_rtrg = target(get_toy_bbs_data(route, region),
-                transform = map(route = !!rts, region = !!rgs))
-)
+# This should be equivalent to the MATSS call below.
+# rts <- c(1, 268, 314)
+# rgs <- c(11, 8, 27)
+#
+# bbs_dats <- drake_plan(
+#   bbs_data_rtrg = target(get_toy_bbs_data(route, region),
+#                 transform = map(route = !!rts, region = !!rgs))
+# )
+
+bbs_dats <- MATSS::build_bbs_datasets_plan(data_subset = c(1, 410, 1977))
+
 
 fs_plan <- drake_plan(
   spab = target(make_spab(dat, datname),
